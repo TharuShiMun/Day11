598c2f635dcc5181a8aab607043c64be841b2598
TharuShiMun <139700921+TharuShiMun@users.noreply.github.com>
2025-01-05 15:13:43 +0000

    updated AboutUs.js function

---
+++ firstapp/components/AboutUs.js
@@ -0,0 +1,11 @@
+import {useNavigation} from '@react-navigation/native';
+import { StyleSheet,View,ScrollView,Image} from 'react-native';
+import {PaperProvider,Text,Divider,Button} from 'react-native-paper';
+
+export default function AboutUs(){
+    return(
+        <>
+        
+        </>
+    );
+}
